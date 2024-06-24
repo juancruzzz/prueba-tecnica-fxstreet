dc87eafd2b0f859bf897997ffcd5cc099e94c7bd
juancruzzz <juancruzmillan11@gmail.com>
2024-06-24 20:24:39 +0200

    webpack

--- webpack.config.ts
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-const __dirname = path.resolve();
-export default {
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/prueba-tecnica-fxstreet/',
-  },
-};
